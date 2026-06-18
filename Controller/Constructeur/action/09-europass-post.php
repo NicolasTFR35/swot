fe817122429f17602b495056c11c39714c58e01f
Nicolas PRAT <145683728+NicolasTFR35@users.noreply.github.com>
2026-06-18 16:25:50 +0200

    Fix Europass Post Mobilité document ID from 87 to 226

--- Controller/Constructeur/action/09-europass-post.php
+++ Controller/Constructeur/action/09-europass-post.php
@@ -5,7 +5,7 @@ if (!is_null($projet["referent_impression_europass_post"])) {
     $label.="<br/>généré le ".$dt->format("d/m/Y à H:i");
 }
 
-$document=$this->getDocument($projet["mobilite_type"], 87, $projet["sejour_versement_bourse"]);
+$document=$this->getDocument($projet["mobilite_type"], 226, $projet["sejour_versement_bourse"]);
 if (!$dropdown) echo '<div class="col-3">';
 ?>
 <a class="<?=($dropdown?"dropdown-item":"")?> text-decoration-none" href="Projet-Document-<?=$projet['id_projet']?>-<?=$document['id']?>" target="_blank">
